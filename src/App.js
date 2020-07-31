a9d6391ca5a99d891f2bf4fbf1cad851ebacee8c
Olaitan Majek <majekolaitan@Olaitans-MacBook.local>
2020-07-31 16:58:01 +0100

    20.2.4 Using a custom data store connector

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ import { Selector } from "./Selector";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
 import { StoreAccess } from "./store/StoreAccess";
+import {
+  CustomConnector,
+  CustomConnectorProvider,
+} from "./store/CustomConnector";
+import { startEditingProduct } from "./store/stateActions";
+import { deleteProduct } from "./store/modelActionCreators";
+import { ProductTable } from "./ProductTable";
+
+const selectors = {
+  products: (store) => store.modelData.products,
+};
+
+const dispatchers = {
+  editCallback: startEditingProduct,
+  deleteCallback: deleteProduct,
+};
 
 export default class App extends Component {
   render() {
@@ -21,6 +37,15 @@ export default class App extends Component {
                 <SupplierDisplay name="Suppliers" />
               </Selector>
             </Provider>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <CustomConnectorProvider dataStore={dataStore}>
+              <CustomConnector selectors={selectors} dispatchers={dispatchers}>
+                <ProductTable />
+              </CustomConnector>
+            </CustomConnectorProvider>
           </div>
         </div>
       </div>
